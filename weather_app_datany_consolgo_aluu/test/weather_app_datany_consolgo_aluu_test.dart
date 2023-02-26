e971c9a367187374c7a74887ec8e8c1cf376ee61
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-02-26 17:43:09 +0300

    done

---
+++ weather_app_datany_consolgo_aluu/test/weather_app_datany_consolgo_aluu_test.dart
@@ -0,0 +1,8 @@
+import 'package:weather_app_datany_consolgo_aluu/weather_app_datany_consolgo_aluu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
