07e29f13d4530eccce170e3d9bbc037c4bda82a5
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-01 23:01:35 +0300

    done

---
+++ variables/bin/variables.dart
@@ -0,0 +1,19 @@
+import 'package:variables/variables.dart' as variables;
+//variables
+
+void main() {}
+// String bul texterdi aluuch maalymattyn turu
+String a = 'men programistmin';
+// int bul butun sandardy aluuch maalymattyn turu
+int b = 10;
+// double bul bolchok sandardy aluuch maalymattyn turu
+double c = 1.4;
+// num bul butun sand jana bolchok sandy alat
+num d = 10;
+num i = 1.5;
+// bool bul logicalyk maalymat turu
+bool f = true;
+bool s = false;
+// dynamic bul dinamicaluu maalymat turu
+dynamic g = true;
+dynamic w = "hello world";
