855fb68a8269415f3e57a15f5c68514404361987
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-05 23:01:18 +0300

    done

--- dart_functions/bin/return.dart
+++ dart_functions/bin/return.dart
@@ -4,9 +4,14 @@ void main() {
   print(result + 10);
   int res = result * 10 + 45;
   print(res);
+  adam(name: "Adilbek", lastName: "Kurmanbek uulu", age: 31);
 }
 
 //Функция белгилүү бир маанини кайтара алат. Бул үчүн, return оператору колдонулат. Мисалы, функциядан эки сандын суммасын кайтаралы:
 int sum(int num1, int num2) {
   return num1 + num2;
+}
+
+void adam({required String name, required String lastName, int? age}) {
+  print("Name: $name, Last Name: $lastName,Age: $age");
 }
