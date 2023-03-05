f257f484608ea52e05e1d25fad91a79213614481
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-05 15:26:35 +0300

    Параметри жок функция

---
+++ dart_function_about_without_parameter/bin/dart_function_about_without_parameter.dart
@@ -0,0 +1,18 @@
+void main() {
+  hello();
+  buy();
+  go();
+}
+
+//Параметри жок функция
+void hello() {
+  print("Hello World!");
+}
+
+void buy() {
+  print("Buy World!");
+}
+
+void go() {
+  print("Go World");
+}
---
+++ dart_function_about_without_parameter/test/dart_function_about_without_parameter_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_function_about_without_parameter/dart_function_about_without_parameter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
