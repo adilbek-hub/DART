d88b9be4d435606b764f242cb00908353d1c2d2f
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-03-09 10:01:33 +0300

    done

---
+++ variables_and_functions/test/variables_and_functions_test.dart
@@ -0,0 +1,8 @@
+import 'package:variables_and_functions/variables_and_functions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
