2615ed704c125bb0f28f084eee3311f5a0bddd72
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-05-10 20:08:14 +0300

    done

---
+++ async_await_weather_app/bin/async_await_weather_app.dart
@@ -0,0 +1,17 @@
+import 'package:test/test.dart';
+
+void main() async {
+  salamBer();
+  koshtosh();
+}
+
+//async убакыт ала турган функция экенин айттык
+//await бир функция бүткөнгө чейин күт
+void salamBer() async {
+  Future.delayed(Duration(seconds: 1)).then((value) => print('Salam2'));
+  print('Salam1');
+}
+
+void koshtosh() {
+  print('Kosh bol');
+}
