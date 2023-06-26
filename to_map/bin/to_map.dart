188b0eb02ba028b4180e69c5fcea19a6d24fde8d
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-06-26 16:15:12 +0300

    do

---
+++ to_map/bin/to_map.dart
@@ -0,0 +1,41 @@
+/*
+Если вы ищете пример того, как этот метод toMap() можно использовать в приложении Flutter,
+вот пример класса, который использует метод toMap():
+
+В этом примере у нас есть класс Book с такими свойствами, как заголовок, описание,
+isCompleted и автор. Метод toMap() определен внутри класса и преобразует экземпляр класса 
+Book в представление карты.
+ */
+class Book {
+  Book({
+    this.title,
+    this.description,
+    this.isComplated,
+    this.author,
+  });
+  String? title;
+  String? description;
+  bool? isComplated;
+  String? author;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'idComplated': isComplated,
+      'author': author,
+    };
+  }
+}
+
+/*Вот пример того, как вы можете использовать метод toMap(): */
+void main() {
+  Book book = Book(
+    title: 'Sabak 1',
+    description: 'ToMap tuuraluu sabak',
+    isComplated: false,
+    author: 'Adilbek',
+  );
+  Map<String, dynamic> bookMap = book.toMap();
+  print(bookMap);
+}
---
+++ to_map/test/to_map_test.dart
@@ -0,0 +1,8 @@
+import 'package:to_map/to_map.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
