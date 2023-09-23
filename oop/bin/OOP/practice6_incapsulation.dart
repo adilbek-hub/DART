bbd29ba4b3cd5057010fdb07a6be954a27beb2e0
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-09-23 14:03:29 +0600

    done

---
+++ oop/bin/OOP/practice6_incapsulation.dart
@@ -0,0 +1,42 @@
+class Person {
+  Person(this._name, this._age);
+  String _name;
+  int _age;
+
+  // Геттеры для чтения данных
+  String get name => _name;
+  int get age => _age;
+
+  // Сеттеры для изменения данных
+  set setName(String newName) {
+    if (newName.isNotEmpty) {
+      _name = newName;
+    }
+  }
+
+  set setAge(int newAge) {
+    if (newAge > 0) {
+      _age = newAge;
+    }
+  }
+
+  // Метод для вывода информации о человеке
+  void displayInfo() {
+    print('Name is: $_name, Age is: $_age');
+  }
+}
+
+void main() {
+  var person = Person('Adilbek', 31);
+
+  // Используем геттеры
+  print('Name is: ${person._name}');
+  print('Age is: ${person._age}');
+
+  // Используем сеттеры для изменения данных
+  print(person._name = 'John');
+  print(person._age = 23);
+
+  // Выводим информацию о человеке
+  person.displayInfo();
+}
