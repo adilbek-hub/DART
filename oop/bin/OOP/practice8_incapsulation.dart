c9222d468558ef4fde2b08a81bba2eca4abe4a8a
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-09-27 13:51:02 +0600

    done

---
+++ oop/bin/OOP/practice8_incapsulation.dart
@@ -0,0 +1,10 @@
+class Student {
+  Student(this._name, this._age, this._studentID);
+  String _name;
+  int _age;
+  String _studentID;
+
+  String get name => _name;
+  int get age => _age;
+  String get StudentId => _studentID;
+}
---
+++ oop/bin/OOP/practice9_extends.dart
@@ -0,0 +1,44 @@
+/*
+Давайте продолжим практику с наследованием. Допустим, у нас есть класс Vehicle (транспортное средство) в качестве класса-родителя, и мы хотим создать классы-потомки Car (автомобиль) и Bicycle (велосипед), которые наследуют свойства и методы от Vehicle.
+Давайте создадим эту иерархию классов:
+ */
+// Класс-родитель
+class Vechicle {
+  Vechicle(this.brand, this.year);
+  String brand;
+  int year;
+
+  void startEngine() {
+    print('$brand Запускает Двигатель');
+  }
+}
+
+// Класс потомок Car
+class Car extends Vechicle {
+  int numberOfDoors;
+  Car(String brand, int year, this.numberOfDoors) : super(brand, year);
+
+  void honkHorn() {
+    print('$brand Сигналить гудком');
+  }
+}
+
+// Класс-потомок Bicycle (велосипед)
+class Bycicle extends Vechicle {
+  Bycicle(String brand, int year) : super(brand, year);
+
+  void ringBell() {
+    print('$brand звонит велоколокольцем.');
+  }
+}
+
+void main() {
+  final car = Car('Toyota', 2022, 4);
+  final bycicle = Bycicle('Kama', 2023);
+  car.startEngine();
+  car.honkHorn();
+
+  bycicle.startEngine();
+  bycicle.ringBell();
+  print(car.brand);
+}
