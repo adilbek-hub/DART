c3611c890c9d005f1caa78f489d068896698cc95
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-07 14:52:57 +0600

    done

---
+++ dart_from_stas/bin/dart_from_stas.dart
@@ -0,0 +1,5 @@
+import 'package:dart_from_stas/dart_from_stas.dart' as dart_from_stas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_from_stas.calculate()}!');
+}
---
+++ dart_from_stas/test/dart_from_stas_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_from_stas/dart_from_stas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
