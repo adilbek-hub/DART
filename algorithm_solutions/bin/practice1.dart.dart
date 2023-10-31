bd088912e49857b289c90494bca7c1d102c336e0
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-10-31 10:22:19 +0600

    вщту

---
+++ algorithm_solutions/bin/practice1.dart.dart
@@ -0,0 +1,9 @@
+void main() {
+  int n = 10;
+  int sum = 0;
+  for (int i = 0; i <= n; i++) {
+    sum += i;
+  }
+
+  print('Сумма чисел от 1 до $n равна $sum');
+}
---
+++ algorithm_solutions/test/algorithm_solutions_test.dart
@@ -0,0 +1,8 @@
+import 'package:algorithm_solutions/algorithm_solutions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
