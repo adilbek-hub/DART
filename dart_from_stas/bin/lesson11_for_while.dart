13e110a4f84a67331acd1668b03b28481a134569
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-11-22 01:52:55 +0600

    вщтк

---
+++ dart_from_stas/bin/lesson11_for_while.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  List<String> students = ['Adil', 'Nura', 'Amantur', 'Bektur'];
+  while (students.isNotEmpty) {
+    students.removeLast();
+    print('55555555555');
+  }
+  print(students);
+}
