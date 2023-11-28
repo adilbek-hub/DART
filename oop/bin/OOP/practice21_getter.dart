30f616a667649c01552a3d99c9374df298d5e636
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-11-28 19:58:56 +0600

    done

--- oop/bin/OOP/practice21_getter.dart
+++ oop/bin/OOP/practice21_getter.dart
@@ -1,1 +1,18 @@
-class NoteBook {}
+class NoteBook {
+  NoteBook(this._name, this._price);
+  final String? _name;
+  final double? _price;
+
+  // Getter method to access private property _name
+  String get name => _name!;
+  // Getter method to access private property _prize
+  double get price => _price!;
+}
+
+void main() {
+  // Create an object of NoteBook class
+  NoteBook nb1 = NoteBook('Acer', 89000);
+  //
+// Объекттин маанилерин көрсөтүү
+  print('${nb1.name}, ${nb1.price}');
+}
