b56a8c704fbed5d86121521b48a6df5fc8dd4d52
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-11-28 19:30:26 +0600

    done

---
+++ dart_from_stas/bin/lesson_16_enum.dart
@@ -0,0 +1,23 @@
+enum Pet { dog, cat }
+
+void main() {
+  final vasiaPet = Pet.cat;
+  final adilPet = Pet.dog;
+
+  print(vasiaPet.index);
+  print(vasiaPet.name);
+
+  _printPetName(vasiaPet);
+  _printPetName(adilPet);
+}
+
+void _printPetName(Pet vasiaPet) {
+  switch (vasiaPet) {
+    case Pet.dog:
+      print('Собака');
+      break;
+    case Pet.cat:
+      print('Кошка');
+      break;
+  }
+}
