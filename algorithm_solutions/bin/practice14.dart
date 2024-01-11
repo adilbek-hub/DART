0fbb39adb7edfad60a5817ab48d5cba8c51bbe27
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-01-11 20:50:52 +0600

    done

--- algorithm_solutions/bin/practice14.dart
+++ algorithm_solutions/bin/practice14.dart
@@ -1,12 +1,13 @@
 //Поиск максимального элемента в массиве:
 void main() {
-  List<int> numbers = [10, 2, 5, 90, 70, 80, 5];
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 12];
+  //intialize
   int max = numbers[0];
+  //Цикл по массиву:
   for (int i = 1; i < numbers.length; i++) {
     if (numbers[i] > max) {
       max = numbers[i];
     }
   }
-
-  print('Максимальный элемент: $max');
+  print(max);
 }
